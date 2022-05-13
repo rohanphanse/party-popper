7dc08f27e2d5b73f52ced372266af7f93177b92f
Roar123 <>
2022-05-13 10:27:00 -0700

    Add express

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const path = require("path")
+const dotenv = require("dotenv")
+
+// Initial
+const app = express()
+dotenv.config()
+
+// Body parser
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")))
+
+// Port
+const PORT = process.env.PORT || 3000
+console.log(PORT)
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
